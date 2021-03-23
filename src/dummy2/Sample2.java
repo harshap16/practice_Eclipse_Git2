495ca80d7811e1705fa656b9e00dcc24dbc98b14
Harsha <harsha.p16@gmail.com>
2021-03-23 20:27:09 +0530

    changes updated

--- src/dummy2/Sample2.java
+++ src/dummy2/Sample2.java
@@ -5,6 +5,19 @@ public class Sample2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		swap1();
+		
+		
 	}
+public static void swap1()
+{
+	int x=100;
+	int y=200;
+	x=x+y;//100+200=300
+	y=x-y;//300-200=100
+	x=x-y;//300-100=200
+	System.out.println("After Swapping : "+ " x="+x+ ",y= "+y);
 
+}
 }
