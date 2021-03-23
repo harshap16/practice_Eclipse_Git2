05b6084455dd8a643c436e1c0aa19a08f8511e8d
Harsha <harsha.p16@gmail.com>
2021-03-23 20:37:16 +0530

    changes updated

--- src/dummy2/Sample2.java
+++ src/dummy2/Sample2.java
@@ -3,11 +3,11 @@ package dummy2;
 public class Sample2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//23rd Mar 2021,Tue
 
 		
-		swap1();
-		
+		//swap1();
+		swap2();
 		
 	}
 public static void swap1()
@@ -19,5 +19,18 @@ public static void swap1()
 	x=x-y;//300-100=200
 	System.out.println("After Swapping : "+ " x="+x+ ",y= "+y);
 
+}
+public static void swap2()
+{
+	String a="Selenium";
+	String b="Webdriver";
+	a=a+b;
+	b=a.substring(0, (a.length()-b.length()));
+	a=a.substring(b.length());
+	System.out.println("a ="+a);
+	System.out.println("b ="+b);
+	
+	
+	
 }
 }
